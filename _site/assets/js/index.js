22cae8a681205298e69ba5f42a78080251de93af
Ryan Rosenfeld <ryro003@gmail.com>
2020-05-16 20:51:08 -0700

    Make a splash with the splash page

---
+++ _site/assets/js/index.js
@@ -0,0 +1,25 @@
+const text = `
+  Welcome! Thanks for swinging by.
+  I'm Ryan. I'm a software engineer... but maybe you already could guess?
+`;
+let i = 0;
+const speed = 50;
+const initialDelay = 500;
+const showLoginDelay = 500;
+
+function typeWelcomeMessage() {
+  if (i < text.length) {
+    const c = text.charAt(i);
+    i++;
+    document.getElementById('welcome-message').innerHTML += c;
+    setTimeout(typeWelcomeMessage, speed);
+  } else {
+    setTimeout(showLogin, showLoginDelay);
+  }
+}
+
+function showLogin() {
+  document.getElementById('login-btn').style.visibility = 'visible';
+}
+
+setTimeout(typeWelcomeMessage, initialDelay);
